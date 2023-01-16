6f1e9054a561d7282f818d2b83f1d8a612885b14
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-01-16 21:33:04 +0100

    treasure tutorial

---
+++ treasure.py
@@ -0,0 +1,19 @@
+#Variables
+#variable name 
+#= assignment/initialization
+# name = "Treasure"
+# print(name)
+# x = 10
+# y = 5
+# mod = x%y
+# print(mod)
+# #Array
+# items = ["Oranges", "Mangoes", "Banana"]
+# print(items[1])
+num1 = int(input("Enter a number "))
+num2 = int(input("Enter a another number "))
+#Conditional statements
+if num1 > num2:
+    print(num1, "is greater than ", num2)
+else:
+    print(num2, "is greater than", num1)
